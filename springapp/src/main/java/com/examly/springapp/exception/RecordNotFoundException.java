472ea65c3e8751f89657eaed9bb931c5056e8f24
sankeerthanasreeramula <sankeerthanasreeramula@gmail.com>
2022-03-27 13:10:15 +0000

    commit changes

--- springapp/src/main/java/com/examly/springapp/exception/RecordNotFoundException.java
+++ springapp/src/main/java/com/examly/springapp/exception/RecordNotFoundException.java
@@ -5,13 +5,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class RecordNotFoundException extends Exception {
-
 	private static final long serialVersionUID = 1L;
-	
 	public RecordNotFoundException(String message) {
 		super(message);
 	}
-	
 	public RecordNotFoundException(String message, Throwable t) {
 		super(message, t);
 	}
