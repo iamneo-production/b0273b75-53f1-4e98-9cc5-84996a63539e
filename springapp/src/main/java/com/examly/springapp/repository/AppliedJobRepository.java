aef09e1f16c170789e02f73ff44b5b5aeed7e502
Adarsh Sankar <61370366+adarsh-2002@users.noreply.github.com>
2022-03-28 20:15:16 +0530

    Implemented deleteAppliedJob API

--- springapp/src/main/java/com/examly/springapp/repository/AppliedJobRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/AppliedJobRepository.java
@@ -7,10 +7,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.examly.springapp.model.AppliedJobModel;
 import com.examly.springapp.model.JobModel;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AppliedJobRepository extends JpaRepository<AppliedJobModel, Long>{
 
 	List<AppliedJobModel> findByEmployeeId(String employeeId);
+
+	@Query("SELECT j FROM AppliedJobModel j WHERE employeeId=employeeId AND jobId=jobId")
+	Optional<AppliedJobModel> findAppliedJob(String employeeId, Long jobId);
 
 	
 }
